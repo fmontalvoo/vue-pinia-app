2fd87f73878b280b1ef936e9eb7fdb5971320e30
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2023-02-27 15:21:29 -0500

    Estructura clientes
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

---
+++ src/modules/clients/components/ClientList.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <ul>
+        <li>
+            <router-link :to="{ name: 'client-detail', params: { id: 1 } }">Cliente 1</router-link>
+        </li>
+    </ul>
+</template>
--- src/modules/clients/pages/ListPage.vue
+++ src/modules/clients/pages/ListPage.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
+import ClientList from '../components/ClientList.vue';
+
 </script>
 
 <template>
     <h1>Clientes</h1>
+    <ClientList />
 </template>
