5a53a19c2c2029afd647051a171610fad0a9ff19
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2023-02-27 17:06:43 -0500

    Modal de carga
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/modules/clients/pages/ListPage.vue
+++ src/modules/clients/pages/ListPage.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import ClientList from '../components/ClientList.vue';
-import PaginationComponent from '../components/PaginationComponent.vue';
+import ClientList from '../components/ClientList.vue'
+import PaginationComponent from '../components/PaginationComponent.vue'
+import LoadingModal from '@/shared/components/LoadingModal.vue'
 
 import { useClients } from '../composables/useClients';
 
@@ -13,4 +14,6 @@ const { clients, currentPage, isLoading, totalPages, getPage } = useClients()
     <ClientList :clients="clients" />
 
     <PaginationComponent :current-page="currentPage" :total-pages="totalPages" @page-changed="getPage" />
+
+    <LoadingModal v-if="isLoading" />
 </template>
